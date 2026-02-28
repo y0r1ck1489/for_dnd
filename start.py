036896928de082cbafcd26a43cc3b8a05937f95e
y0r1ck1489 <lksndrbaglaj@gmail.com>
2026-03-01 01:49:59 +0300

    разработка_gui

--- start.py
+++ start.py
@@ -1,3 +1,18 @@
 from tkinter import *
 from tkinter import ttk
-#хз что это потом доделаю
+ 
+root = Tk()
+root.title("HeroMaker")
+root.geometry("500x500") 
+ 
+def click():
+    window = Tk()
+    window.title("Новое окно")
+    window.geometry("250x200")
+ 
+button = ttk.Button(text="Автоматический режим", command=click)
+button.pack(anchor=CENTER, expand=1)
+button = ttk.Button(text="Ручная сборка", command=click)
+button.pack(anchor=CENTER, expand=2)
+ 
+root.mainloop()
