9bfca19bc45f1f85361acb27e8ee7c7b48f6fdb7
y0r1ck1489 <lksndrbaglaj@gmail.com>
2026-03-01 21:30:39 +0300

    продолжаюразработкуюи

--- start.py
+++ start.py
@@ -3,16 +3,16 @@ from tkinter import ttk
  
 root = Tk()
 root.title("HeroMaker")
-root.geometry("500x500") 
+root.geometry("1200x600") 
  
 def click():
     window = Tk()
     window.title("Новое окно")
     window.geometry("250x200")
  
-button = ttk.Button(text="Автоматический режим", command=click)
-button.pack(anchor=CENTER, expand=1)
-button = ttk.Button(text="Ручная сборка", command=click)
-button.pack(anchor=CENTER, expand=2)
+button = ttk.Button(text="Вперед", command=click)
+button.pack(anchor=E, expand=1)
+button = ttk.Button(text="Назад", command=click)
+button.pack(anchor=E, expand=2)
  
 root.mainloop()
